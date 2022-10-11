09d18c0f419c54ef2a5cab03e1b89edb41dcf2fa
Jackson <deltarax@gmail.com>
2022-10-11 09:38:17 -0700

    Added invisible top and bottom to player

--- main.js
+++ main.js
@@ -35,25 +35,52 @@ let nextWallDist;
  */
 let stretcher;
 
+//creating the invisible top and bottom of the player
+/**
+ * @type {{
+ * posX: number, 
+ * posY: number, 
+ * }}
+ */
+let sTop;
+
+/**
+ * @type {{
+ * posX: number, 
+ * posY: number, 
+ * }}
+ */
+let sBot;
+
 function update() {
   if (!ticks) {
     wall1 = [{pos: vec(50, 0), width: 3, height:15 }];
     wall2 = [{pos: vec(50, 100), width: 3, height: -15}];
     stretcher = {height: 5, posX: 10, posY: 50};
+    sTop = {posX: 10, posY: 48}
+    sBot = {posX: 10, posY: 52}
     nextWallDist = 0;
   }
 
   const scr = difficulty * 0.5;
 
-  if(input.isPressed){
+  //added a cap to the max height
+  if(input.isPressed && stretcher.height < 100){
     stretcher.height++;
+    sTop.posY--;
+    sBot.posY++;
   }else if(stretcher.height > 5 && !input.isPressed){
     stretcher.height--;
+    sTop.posY++;
+    sBot.posY--;
   }
   color("black");
   box(stretcher.posX, stretcher.posY, 1, stretcher.height);
   nextWallDist -= scr;
   if(nextWallDist < 0){
+    const isCollidingWithPlayer = (
+      wall1.height > sTop.posY || wall2.height < sBot.posY
+    )
     const w1 = {pos: vec(100, 0),   width: 3, height: rnd(0,40) };
     const w2 = {pos: vec(100, 100), width: 3, height: rnd(0, -40)};
     wall1.push(w1);
@@ -72,6 +99,9 @@ function update() {
     rect(w2.pos, w2.width, w2.height);
     return w2.pos.x < w2.height;
   });
+  // const isCollidingWithPlayer = (
+  //   wall1.height > sTop.posY || wall2.height < sBot.posY
+  // )
 }
 
 addEventListener("load", onLoad);
